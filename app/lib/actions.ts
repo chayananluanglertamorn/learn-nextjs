6e8e6700eb4e1636d53d58035ffae78e825b885b
chayananluanglertamorn <chayananluanglertamorn@gmail.com>
2024-08-28 15:37:03 +0700

    Validate and prepare the data - Storing values in cents

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,6 +18,7 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   })
+  const amountInCents = amount * 100;
   // Test it out:
-  console.log(typeof customerId, typeof amount, typeof status)
+  console.log(amount, amountInCents)
 }
